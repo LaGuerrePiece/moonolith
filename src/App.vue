d8e2069eea4f264cdfd0251b0fe70ed809fc31f1
white_rqbbit <wedd13@gmail.com>
2022-02-26 19:57:03 +0100

    Simple Grid

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld/>
   </div>
 </template>
 
@@ -23,6 +22,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,42 +1,93 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+var util = require('util')
+var css = require('dom-css')
+var grid = require('pixel-grid')
+var parse = require('parse-color')
+var position = require('mouse-position')
+
+document.body.style.transition = '0.3s all'
+
+var rows = 65
+var columns = 120
+
+var data = []
+
+for (var i = 0; i < rows; i++) {
+  for (var j = 0; j < columns; j++) {
+    data.push([0, 0, 0])
   }
 }
+
+
+function drawOverlay (array) {
+  array.forEach(function (el) {
+    data[(el[0]) * columns + el[1]] = [1, 1, 1]
+  })
+}
+
+var arrow = []
+
+arrow = arrow.map(function (el) {
+  return [el[0] + Math.floor(rows - 10), el[1] + Math.floor(columns - 8)]
+})
+
+var pixels = grid(data, {
+  root: document.body,
+  rows: rows,
+  columns: columns,
+  size: 15,
+  padding: 1,
+  background: [0, 0, 0],
+  formatted: true
+})
+
+// pixels.canvas.style.marginLeft = (window.innerWidth * 0.03) / 2 + 'px'
+// pixels.canvas.style.marginTop = (window.innerHeight * 0.04) / 2 + 'px'
+
+var mouse = position(pixels.canvas)
+
+var row, column, rand, color
+var hue = 0
+
+var started = false
+
+mouse.on('move', function () {
+  if (!started) started = true
+  row = Math.floor(mouse[1] / 16)
+  column = Math.floor(mouse[0] / 16)
+  if (row < rows && column < columns) {
+    hue = (hue + 1) % 360
+    color = parse('hsl(' + hue + ',50, 50)').rgb
+    // document.body.style.background = util.format('rgb(%s,%s,%s)', color[0], color[1], color[2])
+    color = color.map(function (d) { return d / 50 })
+
+    for (var i = -1; i < 2; i++) {
+      for (var j = -1; j < 2; j++) {
+        data[Math.min((row + i) * columns + (column + j), data.length)] = color
+        data[Math.min((row + i + 1) * columns - (column + j), data.length)] = color
+      }
+    }
+  }
+})
+
+pixels.frame(function () {
+  drawOverlay(arrow)
+  for (var i = 0; i < data.length; i++) {
+    rand = Math.random() * 0.02
+    data[i] = [
+      data[i][0] * 0.95 + rand,
+      data[i][1] * 0.95 + rand,
+      data[i][2] * 0.95 + rand
+    ]
+  }
+  pixels.update(data)
+})
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -53,6 +104,6 @@ li {
   margin: 0 10px;
 }
 a {
-  color: #42b983;
+  color: #bb0d0d;
 }
 </style>
