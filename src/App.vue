fd00114e8349f01be4ec794739f0483a744a3158
white_rqbbit <wedd13@gmail.com>
2022-02-27 16:13:40 +0100

    save/load added

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <PixelGrid />
+    <PixelGrid :currentColor="colors"/>
     <ToolBar />
     <Twitter v-model="colors" /> 
   </div>
@@ -11,7 +11,14 @@ import PixelGrid from "./components/PixelGrid.vue";
 import ToolBar from "./components/ToolBar.vue";
 import { Twitter } from "vue-color";
 
-var colors = { r: 255, g: 0, b: 0 }
+var colors = {
+  hex: '#194d33',
+  hex8: '#194D33A8',
+  hsl: { h: 150, s: 0.5, l: 0.2, a: 1 },
+  hsv: { h: 150, s: 0.66, v: 0.30, a: 1 },
+  rgba: { r: 25, g: 77, b: 51, a: 1 },
+  a: 1
+}
 
 export default {
   name: "App",
--- src/components/PixelGrid.vue
+++ src/components/PixelGrid.vue
@@ -1,107 +1,144 @@
 <template>
-  <div>
-  </div>
+  <div></div>
 </template>
 
 <script>
-var util = require('util')
-var css = require('dom-css')
-var grid = require('pixel-grid')
-var parse = require('parse-color')
-var position = require('mouse-position')
-
-document.body.style.transition = '0.3s all'
-
-var rows = 90
-var columns = 60
-
-var data = []
-var persistentData = []
-
-for (var i = 0; i < rows; i++) {
-  for (var j = 0; j < columns; j++) {
-    data.push([0, 0, 0])
-    persistentData.push([[0, 0, 0], 0])
-  }
-}
-
-var pixels = grid(data, {
-  root: document.body,
-  rows: rows,
-  columns: columns,
-  size: 15,
-  padding: 1,
-  background: [0, 0, 0],
-  formatted: true
-})
-
-// pixels.canvas.style.marginLeft = (window.innerWidth * 0.03) / 2 + 'px'
-// pixels.canvas.style.marginTop = (window.innerHeight * 0.04) / 2 + 'px'
-
-var mouse = position(pixels.canvas)
-
-var mousex, mousey, rand, color
-var hue = 0
-var toggleDraw = false
-
-document.body.addEventListener("mousedown", () => {
-  toggleDraw = true
-})
-
-document.body.addEventListener("mouseup", () => {
-  toggleDraw = false
-})
-
-function pen(color)
-{
-  var screenx = document.documentElement.clientWidth
-  var screeny = document.documentElement.clientHeight
-  var pixelSize = screenx / columns
-  var nbPixely = screeny / pixelSize
-  mousex = Math.floor((mouse[0] / screenx) * columns )
-  mousey = Math.floor((mouse[1] / screeny) * nbPixely)
-
-  if (mousex < rows && mousey < columns) {
-    draw_pixel(mousex, mousey, color)
-  }
-}
-
-function draw_persistent_data()
-{
-    for (var i = 0; i < data.length; i++) {
-      if (persistentData[i][1] != 0)
-        data[i] = persistentData[i][0]
+export default {
+  name: "PixelGrid",
+  props: {
+    currentColor: Object,
+  },
+  components: {},
+  data() {
+    return {
+      grid_data: []
+    };
+  },
+  methods: {
+    load() {
+      if (localStorage.getItem("grid_data")) {
+        return JSON.parse(localStorage.getItem("grid_data"));
+      }
+      return []
+    },
+    save(new_data){
+      localStorage.setItem("grid_data", JSON.stringify(new_data))
     }
-}
+  },
+  mounted() {
+    var util = require("util");
+    var css = require("dom-css");
+    var grid = require("pixel-grid");
+    var parse = require("parse-color");
+    var position = require("mouse-position");
 
-function draw_noise(){
-  for (var i = 0; i < data.length; i++) {
-    rand = Math.random() * 0.02
-    data[i] = [
-      data[i][0] * 0.95 + rand,
-      data[i][1] * 0.95 + rand,
-      data[i][2] * 0.95 + rand
-    ]
-  }
-}
+    document.body.style.transition = "0.3s all";
 
-pixels.frame(function () {
-  draw_noise()
-  if (toggleDraw)
-    pen([0.2, 0.3, 0.4])
-  draw_persistent_data()
-  pixels.update(data)
-})
-pixels.canvas.style.width = "100%"
+    var rows = 90;
+    var columns = 60;
 
-function draw_pixel(x, y, color){
-  pos = y * columns + x
-  persistentData[pos][0] = color
-  persistentData[pos][1] = 1 //marqueur "dessiné"
-}
+    var data = [];
+    var persistentData = this.load();
+    var fillPersitent = false
+
+
+    if(persistentData.length == 0)
+      fillPersitent = true
+
+    window.addEventListener('beforeunload', () => {
+      this.save(persistentData)
+    })
+
+    for (var i = 0; i < rows; i++) {
+      for (var j = 0; j < columns; j++) {
+        data.push([0, 0, 0]);
+        if (fillPersitent)
+          persistentData.push([[0, 0, 0], 0]); //[color, displayed]
+      }
+    }
+    var pixels = grid(data, {
+      root: document.body,
+      rows: rows,
+      columns: columns,
+      size: 15,
+      padding: 1,
+      background: [0, 0, 0],
+      formatted: true,
+    });
+
+    // pixels.canvas.style.marginLeft = (window.innerWidth * 0.03) / 2 + 'px'
+    // pixels.canvas.style.marginTop = (window.innerHeight * 0.04) / 2 + 'px'
+
+    var mouse = position(pixels.canvas);
+
+    var mousex, mousey, rand, color;
+    var hue = 0;
+    var toggleDraw = false;
+
+    document.body.addEventListener("mousedown", () => {
+      toggleDraw = true;
+    });
+
+    document.body.addEventListener("mouseup", () => {
+      toggleDraw = false;
+    });
+
+    function pen(color) {
+      var screenx = document.documentElement.clientWidth;
+      var screeny = document.documentElement.clientHeight;
+      var pixelSize = screenx / columns;
+      var nbPixely = screeny / pixelSize;
+      mousex = Math.floor((mouse[0] / screenx) * columns);
+      mousey = Math.floor((mouse[1] / screeny) * nbPixely);
+
+      if (mousex < rows && mousey < columns) {
+        draw_pixel(mousex, mousey, color);
+      }
+    }
+
+    function draw_persistent_data() {
+      for (var i = 0; i < data.length; i++) {
+        if (persistentData[i][1] != 0) data[i] = persistentData[i][0];
+      }
+    }
+
+    function draw_noise() {
+      for (var i = 0; i < data.length; i++) {
+        rand = Math.random() * 0.02;
+        data[i] = [
+          data[i][0] * 0.95 + rand,
+          data[i][1] * 0.95 + rand,
+          data[i][2] * 0.95 + rand,
+        ];
+      }
+    }
+
+    var component = this;
+
+    pixels.frame(function () {
+      draw_noise();
+      if (toggleDraw) pen(component.currentColor);
+      draw_persistent_data();
+      pixels.update(data);
+    });
+    pixels.canvas.style.width = "100%";
+
+    function draw_pixel(x, y, color) {
+      var pos = y * columns + x;
+      color = to_color_array(color);
+      persistentData[pos][0] = color;
+      persistentData[pos][1] = 1; //marqueur "dessiné"
+    }
+
+    function to_color_array(color) {
+      color = color.rgba;
+      color = [color.r / 255, color.g / 255, color.b / 255];
+      return color;
+    }
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
 </style>
