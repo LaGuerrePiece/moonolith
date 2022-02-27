d4648451f419a16feccbadde2a198f902dffafab
white_rqbbit <wedd13@gmail.com>
2022-02-27 17:59:34 +0100

    load png buffer lol

--- src/components/PixelGrid.vue
+++ src/components/PixelGrid.vue
@@ -11,7 +11,7 @@ export default {
   components: {},
   data() {
     return {
-      grid_data: []
+      grid_data: [],
     };
   },
   methods: {
@@ -19,11 +19,11 @@ export default {
       if (localStorage.getItem("grid_data")) {
         return JSON.parse(localStorage.getItem("grid_data"));
       }
-      return []
+      return [];
     },
-    save(new_data){
-      localStorage.setItem("grid_data", JSON.stringify(new_data))
-    }
+    save(new_data) {
+      localStorage.setItem("grid_data", JSON.stringify(new_data));
+    },
   },
   mounted() {
     var util = require("util");
@@ -31,6 +31,8 @@ export default {
     var grid = require("pixel-grid");
     var parse = require("parse-color");
     var position = require("mouse-position");
+    var fs = require("fs");
+    var PNG = require("pngjs").PNG;
 
     document.body.style.transition = "0.3s all";
 
@@ -39,21 +41,18 @@ export default {
 
     var data = [];
     var persistentData = this.load();
-    var fillPersitent = false
+    var fillPersitent = false;
 
+    if (persistentData.length == 0) fillPersitent = true;
 
-    if(persistentData.length == 0)
-      fillPersitent = true
-
-    window.addEventListener('beforeunload', () => {
-      this.save(persistentData)
-    })
+    window.addEventListener("beforeunload", () => {
+      this.save(persistentData);
+    });
 
     for (var i = 0; i < rows; i++) {
       for (var j = 0; j < columns; j++) {
         data.push([0, 0, 0]);
-        if (fillPersitent)
-          persistentData.push([[0, 0, 0], 0]); //[color, displayed]
+        if (fillPersitent) persistentData.push([[0, 0, 0], 0]); //[color, displayed]
       }
     }
     var pixels = grid(data, {
@@ -83,16 +82,26 @@ export default {
       toggleDraw = false;
     });
 
-    function pen(color) {
+    function mousePosOnGrid() {
       var screenx = document.documentElement.clientWidth;
       var screeny = document.documentElement.clientHeight;
       var pixelSize = screenx / columns;
       var nbPixely = screeny / pixelSize;
       mousex = Math.floor((mouse[0] / screenx) * columns);
       mousey = Math.floor((mouse[1] / screeny) * nbPixely);
+    }
 
+    function pen(color) {
+      mousePosOnGrid();
       if (mousex < rows && mousey < columns) {
         draw_pixel(mousex, mousey, color);
+      }
+    }
+
+    function eraser() {
+      mousePosOnGrid();
+      if (mousex < rows && mousey < columns) {
+        erase_pixel(mousex, mousey);
       }
     }
 
@@ -113,6 +122,75 @@ export default {
       }
     }
 
+    // function loadImage(imgURL) {
+    //   //ajouter nbPixel en entrée
+
+    //   fetch(imgURL)
+    //     .then((res) => res.blob())
+    //     .then((blob) => {
+    //       // Here's where you get access to the blob
+    //       // And you can use it for whatever you want
+    //       // Like calling ref().put(blob)
+    //       blob.arrayBuffer().then((buffer) => {
+    //         const array = new Uint8Array(buffer);
+    //         console.log(array[0]);
+    //         console.log(blob);
+    //       });
+    //     });
+    // }
+
+    function getBase64FromImageUrl(url) {
+      var img = new Image();
+
+      img.setAttribute("crossOrigin", "anonymous");
+
+      img.onload = function () {
+        var canvas = document.createElement("canvas");
+        canvas.width = this.width;
+        canvas.height = this.height;
+
+        var ctx = canvas.getContext("2d");
+        ctx.drawImage(this, 0, 0);
+
+        var dataURL = canvas.toDataURL("image/png");
+
+        var array = (
+          Buffer.from(
+            dataURL.replace(/^data:image\/(png|jpg);base64,/, ""),
+            "base64"
+          )
+        );
+
+        for (let y = 0; y < 32; y++) {
+          for (let x = 0; x < 32; x++) {
+            let idx = 32 * y + x;
+            console.log(
+              "array [" +
+                idx +
+                "] = " +
+                array[idx] +
+                "," +
+                array[idx + 1] +
+                "," +
+                array[idx + 2]
+            );
+            draw_pixel(x, y, {
+              rgba: {
+                r: array[idx],
+                g: array[idx + 1],
+                b: array[idx + 2],
+              },
+            });
+          }
+        }
+      };
+
+      img.src = url;
+    }
+
+    getBase64FromImageUrl("https://i.imgur.com/qAhwWr9.png");
+    // loadImage("https://i.imgur.com/qAhwWr9.png");
+
     var component = this;
 
     pixels.frame(function () {
@@ -128,6 +206,12 @@ export default {
       color = to_color_array(color);
       persistentData[pos][0] = color;
       persistentData[pos][1] = 1; //marqueur "dessiné"
+    }
+
+    function erase_pixel(x, y) {
+      var pos = y * columns + x;
+      persistentData[pos][0] = [0, 0, 0];
+      persistentData[pos][1] = 0; //marqueur "dessiné"
     }
 
     function to_color_array(color) {
