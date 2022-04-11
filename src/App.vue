898ddb3761b487f7c13fbb2eff1b509281d93053
white_rqbbit <wedd13@gmail.com>
2022-04-11 10:32:54 +0200

    fix: revert App changes

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,9 @@
 import { ref } from 'vue';
 
 import PixelsGrid from './components/PixelsGrid.vue';
-import PixelsGrid4Single from './components/PixelsGrid4Single.vue';
 import ToolBar from './components/ToolBar.vue';
 import Palette from './components/Palette.vue';
 import Draggable from './components/Draggable.vue';
-
-import Api from './Api.vue'
 
 import Tool from './models/tools';
 
@@ -16,19 +13,10 @@ const color = ref('');
 const hasBought = ref(0);
 const onDelete = ref(0);
 const importedImage = ref();
-console.log(window.location.pathname);
-const urlData = window.location.pathname.split("/");
-const canva = false;
-if(urlData[1] == 'runes')
-{
-    console.log("It's canva");
-    canva = true;
-}
-
 </script>
 
 <template>
-    <Draggable color="white" style=""  v-if="!canva">
+    <Draggable color="white">
         <ToolBar
             @toolChanged="tool = $event"
             @import="importedImage = $event"
@@ -43,18 +31,7 @@ if(urlData[1] == 'runes')
             style="margin: auto"
         />
     </Draggable>
-    <PixelsGrid v-if="!canva"
-        @contextmenu.prevent
-        :tool="tool"
-        :color="color"
-        :importedImage="ref(importedImage)"
-        :hasBought="ref(hasBought)"
-        @boughtBack="hasBought = 0"
-        :onDelete="ref(onDelete)"
-        @deleteBack="onDelete = 0"
-        v-model:changeColor="color"
-    />
-    <PixelsGrid4Single v-else
+    <PixelsGrid
         @contextmenu.prevent
         :tool="tool"
         :color="color"
