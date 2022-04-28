48aa05dadf25e6d76c17b167f21f17b6b16d8916
white_rqbbit <wedd13@gmail.com>
2022-04-28 16:33:49 +0200

    refactor: shortcuts for ctrl+Z

--- src/models/tools.js
+++ src/models/tools.js
@@ -35,12 +35,8 @@ let colorPicked2 = Const.RGB8;
 
 //prettier-ignore
 export function keyManager(e){
-    if (e.metaKey && e.key === 'z') {console.log('undo()'); return}
-    if (e.ctrlKey && e.key === 'z') {console.log('undo()'); return}
-    if (e.ctrlKey && e.key === 'Z') {console.log('redo()'); return}
-    if (e.metaKey && e.key === 'Z') {console.log('redo()'); return}
-    if (e.ctrlKey && e.key === 'y') {console.log('redo()'); return}
-    if (e.metaKey && e.key === 'y') {console.log('redo()'); return}
+    if ((e.metaKey || e.ctrlKey) && e.key === 'z') {console.log('undo()'); return}
+    if ((e.metaKey || e.ctrlKey ) && (e.key === 'Z' || e.key === 'y')) {console.log('redo()'); return}
     if (e.key === 'x') eraseAllPixel();
     if (e.key === 'c') console.log('Total H', Const.COLUMNS, 'Total W', Const.LINES, 'render W', renderWidth, 'render H', renderHeight, 'viewPosX', viewPosX, 'viewPosY', viewPosY);
     if (e.key === 'm') { moveDrawing(50, 400) }
