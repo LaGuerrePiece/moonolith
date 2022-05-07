1f90219c5943724c1a68a80611773dc389357862
Stormower <nicolasletimon@gmail.com>
2022-05-07 23:41:34 +0200

    new contract

--- src/utils/web3.js
+++ src/utils/web3.js
@@ -8,7 +8,7 @@ import { displayShareScreen } from '../models/display';
 
 const provider = new ethers.providers.InfuraProvider('rinkeby');
 const iface = new Interface(contractABI);
-const contractAddress = '0x0B4D88a3325830e4F414865Ff68B55CAdb927614';
+const contractAddress = '0x89d7a208e36F616D79f87d17e51cB6489e7be9b0';
 const contract = new ethers.Contract(contractAddress, contractABI, provider);
 let metamaskProvider;
 var metamaskContract;
